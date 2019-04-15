5aad7c5d25f21a6ae6ceae48dcdf93ff763438ae
Romain Dannin <romain.dannin@gmail.com>
2019-04-15 11:37:21 +0200

    probuilder

---
+++ Assets/Romain/automaticDoor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class automaticDoor : MonoBehaviour
+{
+
+    public Animator doorAnim; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            doorAnim.SetTrigger("isOpen");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            doorAnim.SetTrigger("isClosed");
+        }
+    }
+}
