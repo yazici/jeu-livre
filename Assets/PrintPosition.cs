5e6ca269863d72553d66a2ca3ade93dec1c8e7be
Romain Dannin <romain.dannin@gmail.com>
2019-04-19 17:48:50 +0200

    ui robot

---
+++ Assets/PrintPosition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class PrintPosition : MonoBehaviour
+{
+    public Text y;
+    public Text x;
+
+    private float yRotation; 
+    private float xRotation;
+
+    public GameObject player;
+    private Camera cam; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yRotation = Mathf.Round(player.transform.rotation.y * 180);
+        xRotation = Mathf.Round(cam.transform.rotation.x * 180);
+
+        y.text = "y = " + yRotation;
+        x.text = "x = " + xRotation;
+    }
+}
