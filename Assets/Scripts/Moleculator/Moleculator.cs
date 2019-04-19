fdb39b1b490e38255658c216cbe8b237d4497f57
Camille <camille.durif@edu.gobelins.fr>
2019-04-19 11:35:42 +0200

    Clean moleculator

--- Assets/Scripts/Moleculator/Moleculator.cs
+++ Assets/Scripts/Moleculator/Moleculator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Moleculator : MonoBehaviour
 {
@@ -10,6 +11,12 @@ public class Moleculator : MonoBehaviour
     public RectTransform dropZone1;
     public RectTransform dropZone2;
 
+    public GameObject[] goodItems;
+
+    public Text consoleText;
+    public Button validateButton;
+
+    private int score = 0;
     private static GameObject[] droppedItems;
 
 
@@ -17,6 +24,7 @@ public class Moleculator : MonoBehaviour
     void Start()
     {
         droppedItems = new GameObject[3];
+        validateButton.interactable = false;
     }
 
     // Update is called once per frame
@@ -66,6 +74,16 @@ public class Moleculator : MonoBehaviour
         {
             gameObject.transform.localPosition = Vector3.zero;
         }
+
+        if(droppedItems[0] != null && droppedItems[1] != null && droppedItems[2] != null)
+        {
+            validateButton.interactable = true;
+        }
+        else
+        {
+            validateButton.interactable = false;
+        }
+
     }
    
 
@@ -99,6 +117,8 @@ public class Moleculator : MonoBehaviour
             }
 
         }
+
+        validateButton.interactable = false;
     }
 
     public void doReset()
@@ -118,5 +138,50 @@ public class Moleculator : MonoBehaviour
             droppedItems[2].transform.localPosition = Vector3.zero;
             droppedItems[2] = null;
         }
+
+        validateButton.interactable = false;
+
+    }
+
+    public void Validate()
+    {
+
+        score = 0;
+
+        for(int i=0; i<3; i++)
+        {
+            for (int j=0; j<3; j++) {
+
+                if (droppedItems[i] == goodItems[j])
+                {
+                    score++;
+                }
+            }
+        }
+
+        switch (score)
+        {
+            case 0:
+                consoleText.text += "\n\nAnalyse de la solution...\n\nRésultat : solution valide à 0%";
+                break;
+            case 1:
+                consoleText.text += "\n\nAnalyse de la solution...\n\nRésultat : solution valide à 33%";
+                break;
+            case 2:
+                consoleText.text += "\n\nAnalyse de la solution...\n\nRésultat : solution valide à 66%";
+                break;
+            case 3:
+                consoleText.text += "\n\nAnalyse de la solution...\n\nRésultat : solution valide à 100%";
+                break;
+            default:
+                consoleText.text += "\n\nAnalyse de la solution...\n\nERREUR - Veuillez recommencer la procédure";
+                break;
+        }
+
+    }
+
+    public void setFocus(Transform window)
+    {
+        window.SetSiblingIndex(1);
     }
 }
