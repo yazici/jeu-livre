ca38556eb10186caac8cca703913907f127dc406
Baptiste Perraud <baptiste.perraud@orange.fr>
2019-04-25 13:43:42 +0200

    Scripts cleaning

---
+++ Assets/Scripts/Molecules/MoleculatorTrigger.cs
@@ -0,0 +1,24 @@
+﻿using Interactions;
+using UnityEngine;
+
+namespace Molecules
+{
+    public class MoleculatorTrigger : Trigger
+    {
+
+        private LoadInterface m_LoadInterface;
+
+        private new void Start()
+        {
+            base.Start();
+            m_LoadInterface = GameObject.FindWithTag("UIManager").GetComponent<LoadInterface>();
+        }
+
+        protected override void BeforeTrigger()
+        {
+            // Don't open interface if it's already opened
+            if (Cursor.lockState == CursorLockMode.None) return;
+            StartCoroutine(m_LoadInterface.OpenInterface("moleculatorUI"));
+        }
+    }
+}
