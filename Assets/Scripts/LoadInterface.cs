b7fbe431df51d7f5a311404df59d848bc4ca559e
Romain Dannin <romain.dannin@gmail.com>
2019-04-25 16:51:05 +0200

    clean + merge

--- Assets/Scripts/LoadInterface.cs
+++ Assets/Scripts/LoadInterface.cs
@@ -23,7 +23,7 @@ public class LoadInterface : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
-            // StartCoroutine(OpenInterface("moleculatorUI"));
+           StartCoroutine(OpenInterface("moleculatorUI"));
         }
         if (Input.GetKeyDown(KeyCode.Y) || Input.GetKeyDown(KeyCode.Escape))
         {
