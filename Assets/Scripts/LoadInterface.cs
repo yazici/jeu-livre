cb068aa245e43f04a73c159589471dce91842500
Baptiste Perraud <baptiste.perraud@orange.fr>
2019-04-25 11:50:50 +0200

    Big cleaning of project

---
+++ Assets/Scripts/LoadInterface.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class LoadInterface : MonoBehaviour
+{
+    public Canvas robotUI;
+    public CanvasGroup robotUICanvasGroup;
+
+    private GameObject loadingBar;
+    private Animation loadingBarAnim;
+
+    private GameObject objectInterface;
+
+    private CanvasGroup canvasGroup;
+
+    [SerializeField] private float fadeTime = 1.5f;
+
+    private bool visibleInterface;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            // StartCoroutine(OpenInterface("moleculatorUI"));
+        }
+        if (Input.GetKeyDown(KeyCode.Y) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (visibleInterface)
+                StartCoroutine(CloseInterface());
+        }
+    }
+
+    public IEnumerator OpenInterface(string objectToLoad)
+    {
+        GameManager.ShowCursor();
+
+        StartCoroutine(PlayLoadingAnimation());
+        StartCoroutine(AlphaFadeOut(robotUICanvasGroup, 0.2f, fadeTime));
+
+        yield return WaitForAnimation();
+
+        Destroy(loadingBar);
+
+        yield return StartCoroutine(InstantiateGameObject(objectToLoad, gameObject));
+        yield return StartCoroutine(AlphaFadeIn(canvasGroup, 1f, fadeTime));
+
+        visibleInterface = true;
+    }
+
+    public IEnumerator CloseInterface()
+    {
+        visibleInterface = false;
+        yield return StartCoroutine(AlphaFadeOut(canvasGroup, 0f, fadeTime));
+        GameManager.HideCursor();
+        StartCoroutine(AlphaFadeIn(robotUICanvasGroup, 1f, fadeTime));
+    }
+
+    IEnumerator PlayLoadingAnimation()
+    {
+        Instantiate(Resources.Load("LoadingBar"));
+        loadingBar = GameObject.Find("LoadingBar(Clone)");
+        loadingBar.transform.SetParent(robotUI.transform);
+        loadingBarAnim = loadingBar.GetComponentInChildren<Animation>();
+        loadingBarAnim.Play();
+
+        yield return null;
+    }
+
+
+    IEnumerator WaitForAnimation()
+    {
+        do
+        {
+            yield return null;
+        } while (loadingBarAnim.isPlaying);
+    }
+
+    IEnumerator InstantiateGameObject(string objectToLoad, GameObject gameObjectToLoad)
+    {
+        Instantiate(Resources.Load(objectToLoad));
+        gameObjectToLoad = GameObject.Find(objectToLoad + "(Clone)");
+        gameObjectToLoad.transform.SetParent(robotUI.transform);
+        canvasGroup = gameObjectToLoad.GetComponentInChildren<CanvasGroup>();
+
+        objectInterface = gameObjectToLoad;
+
+        yield return null;
+    }
+
+    IEnumerator AlphaFadeIn(CanvasGroup canvasGroupToFade, float alphaValue, float fadeTime)
+    {
+        while (canvasGroupToFade.alpha < alphaValue)
+        {
+            canvasGroupToFade.alpha += 1 * Time.deltaTime / fadeTime;
+            yield return null;
+        }
+    }
+
+    IEnumerator AlphaFadeOut(CanvasGroup canvasGroupToFade, float alphaValue, float fadeTime)
+    {
+        while (canvasGroupToFade.alpha >= alphaValue)
+        {
+            canvasGroupToFade.alpha -= 1 * Time.deltaTime / fadeTime;
+
+            if (canvasGroupToFade != robotUICanvasGroup)
+            {
+                if (canvasGroupToFade.alpha <= 0)
+                {
+                    Destroy(objectInterface);
+                    yield break;
+                }
+            }
+
+            yield return null;
+        }
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/PrintPosition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class PrintPosition : MonoBehaviour
+{
+    public Text y;
+    public Text x;
+
+    private float yRotation; 
+    private float xRotation;
+
+    private GameObject player;
+    private Camera cam; 
+
+    void Start()
+    {
+        cam = Camera.main; 
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+        yRotation = Mathf.Round(player.transform.rotation.y * 180);
+        xRotation = Mathf.Round(cam.transform.rotation.x * 180);
+
+        y.text = "y = " + yRotation;
+        x.text = "x = " + xRotation;
+    }
+}
