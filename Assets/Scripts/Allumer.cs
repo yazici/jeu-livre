79fb28020f56dd342c302980b99a00a54ec5bdf3
Camille Durif <ca_millou@yahoo.fr>
2019-05-20 18:30:38 +0200

    Fading lights

---
+++ Assets/Scripts/Allumer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Allumer : MonoBehaviour
+{
+
+    public VLight vlight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (vlight.lightMultiplier < 0.4)
+        {
+
+
+            vlight.lightMultiplier += 0.001f;
+        }
+
+
+    }
+}
